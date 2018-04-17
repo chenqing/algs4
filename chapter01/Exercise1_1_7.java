2d30804f49177ad7327c0c0d23e2df8599685d8a
chenqing <chenqing663@gmail.com>
2018-04-17 23:06:25 +0800

    add one

---
+++ chapter01/Exercise1_1_7.java
@@ -0,0 +1,27 @@
+/**
+ * 1.1.7 
+ */
+
+ class Exercise1_1_7 {
+
+  public static void main(String args[]) {
+    double t = 9.0;
+
+    while (Math.abs(t - 9.0/t) > 0.001) {
+      t = (9.0/t + t) / 2.0;
+    }
+
+    System.out.printf("%.5f\n", t);
+
+    int sum = 0;
+    for(int i = 1; i < 1000; i++) {
+      for (int j = 0; j < i ; j++) {
+        sum++;
+      }
+    }
+
+    System.out.println(sum);
+  }
+
+
+ }
